4109be407f108e526c5a6a6a3b2f8d82a33a083c
Gregory Tolmachev <GTollm@icloud.com>
2024-12-10 23:42:15 +0300

    CMake and main added

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 
-#include <string>
 #include <iostream>
+#include <string>
+
+#include "lib/game/game.hpp"
+#include "lib/game_manager/game_manager.hpp"
+#include "lib/writer/writer.hpp"
 
 int main() {
-    while(true) {
-        std::string cmd;
-        std::getline(std::cin, cmd);
+  BattleShipGame::Game game;
+  BattleShipGame::GameManager manager(game);
+  BattleShipGame::ConsoleWriter writer(std::cout);
+  manager.SetWriter(writer);
+  while (true) {
+    std::string cmd;
+    std::getline(std::cin, cmd);
+    manager.ProcessCommand(cmd);
+  }
 
-        if(cmd == "exit") {
-            return 0;
-        } else if(cmd == "ping") {
-            std::cout << "pong" << std::endl;
-        }
-    }
-
-    return 0;
+  return 0;
 }
